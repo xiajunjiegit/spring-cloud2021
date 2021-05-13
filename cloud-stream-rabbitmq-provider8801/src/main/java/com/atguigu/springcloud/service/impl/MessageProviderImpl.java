1a27ff6bf205f2ce620299fd9abfcd16c8b5e058
xiajunjiegit <921723915@qq.com>
2021-05-13 17:30:44 +0800

    springcloud 的stream 生产者消费者及避免重复消费

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -21,7 +21,7 @@ public class MessageProviderImpl implements IMessageProvider {
     @Override
     public String send() {
         String serial = UUID.randomUUID().toString();
-        this.output.send(MessageBuilder.withPayload(serial).build());
+        output.send(MessageBuilder.withPayload(serial).build());
 
         //创建并发送消息
         System.out.println("serial:::"+serial);
