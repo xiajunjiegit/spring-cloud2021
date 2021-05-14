ecd3fe6e9d02df41e061bf7a8a66cb9abf56b903
xiajunjiegit <921723915@qq.com>
2021-05-14 17:26:32 +0800

    springcloud alibaba Nacos 基础配置

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/alibaba/confroller/OrderNacosController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.alibaba.confroller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderNacosController {
+
+    @Value("${service-url.nacos-user-service}")
+    private String serviceUrl;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id){
+        return restTemplate.getForObject(serviceUrl+"/payment/nacos/"+id,String.class);
+    }
+}
